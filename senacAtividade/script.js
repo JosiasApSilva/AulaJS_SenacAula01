866c9ae7774b25341025c6a0553169a29b74d25b
Josias <josias.aparecido@outlook.com>
2023-07-29 12:07:54 -0300

    atualização alerta na div

--- senacAtividade/script.js
+++ senacAtividade/script.js
@@ -4,14 +4,19 @@ const buttonElement = document.getElementById('checkButton');
 
 buttonElement.addEventListener('click', function () {
   const numero = parseInt(document.getElementById('numberInput').value);
+  const paragraphElement = document.getElementById('messengerResult');
+ 
+// // Altera o conteúdo do parágrafo
 
   if (numero <= 10) {
     buttonElement.classList.remove('button-green');
     buttonElement.classList.add('button-red');
-    alert('O número é menor ou igual a 10!');
+    // alert('O número é menor ou igual a 10!');
+    paragraphElement.innerHTML = '<strong>O número é menor ou igual a 10</strong>';
   } else {
     buttonElement.classList.remove('button-red');
     buttonElement.classList.add('button-green');
-    alert('O número é maior que 10!');
+    // alert('O número é maior que 10!');
+    paragraphElement.innerHTML = '<strong>O número é maior que 10</strong>';
   }
 });
